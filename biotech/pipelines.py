371be501e5069894f24521b0ba9751bc4fa9caa1
danielcheng <dongxucheng@gmail.com>
2013-10-15 09:20:40 +0800

    remove xlwt  deps

--- biotech/pipelines.py
+++ biotech/pipelines.py
@@ -3,7 +3,6 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 import csv
-import xlwt
 from biotech.items import BiotechItem
 
 class BiotechPipeline(object):
